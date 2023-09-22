7a4ac9e0a99bacc0c3904ea1f42b8913b0c3f721
Aryaveer <abajpai@contentactive.com>
2023-09-22 21:43:01 +0530

    Initial Commit

---
+++ ThreadsCloneApp/ThreadsCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneAppApp.swift
+//  ThreadsCloneApp
+//
+//  Created by Unthinkable-mac-0077 on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
